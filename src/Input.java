c5cff1f074e1ec996b8708b757b9a88175e78991
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2025-02-01 23:11:28 +0545

    2D Array code

--- src/Input.java
+++ src/Input.java
@@ -7,10 +7,24 @@ public class Input {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int[] arr = new int[5];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = in.nextInt();
+//        int[] arr = new int[5];
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = in.nextInt();
+
+        // System.out.println(Arrays.toString(arr));
+        //
+
+
+        // array of objects
+
+        String[] str = new String[4];
+        for (int i = 0; i < str.length; i++) {
+            str[i] = in.next();
         }
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(str));
+      // modify
+        str[1] = "jibachh";
+
+        System.out.println(Arrays.toString(str));
     }
 }
